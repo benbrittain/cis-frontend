ca9bf64bc85fdccd90fb18d744be2af0f95ac895
Ben Brittain <ben@brittain.org>
2016-01-06 13:12:10 -0500

    newticket

--- index.php
+++ index.php
@@ -1,31 +1,12 @@
-<?php
-require 'vendor/autoload.php';
-include 'config.php';
-
-use Freshdesk\Config\Connection,
-  Freshdesk\Rest,
-  Freshdesk\Ticket,
-  Freshdesk\Model\Contact,
-  Freshdesk\Model\Ticket as TicketM,
-  Freshdesk\Tool\ModelGenerator;
-
-$url = 'https://' . $KEY . ':X@chester.freshdesk.com';
-$conf = new Connection($url);
-
-//basic/general rest calls
-$fd = new Rest($conf);
-//get ticket, this call will be removed from Rest class & moved to Ticket class
-//$json = $fd->getSingleTicket(1701);
-//print_r($json);
-//for ticket-calls:
-$t = new Ticket($conf);
-//create new ticket
-$model = new TicketM(
-    array(
-        'description'   => 'Ignore this ticket, it is a test',
-        'subject'       => 'API-test',
-        'email'         => 'foo@bar.com'
-    )
-);
-//create new ticket, basic example
-$t->createNewTicket($model);
+<html>
+  <head>
+    <title>RIT CIS Ticketing System</title>
+  </head>
+  <body>
+    <form action="newticket.php" method="post">
+      <p>Name: <input type="text" name="name" /></p>
+      <p>Problem: <input type="text" name="problem" /></p>
+      <p><input type="submit" /></p>
+    </form>
+ </body>
+</html>
---
+++ newticket.php
@@ -0,0 +1,31 @@
+<?php
+require 'vendor/autoload.php';
+include 'config.php';
+
+use Freshdesk\Config\Connection,
+  Freshdesk\Rest,
+  Freshdesk\Ticket,
+  Freshdesk\Model\Contact,
+  Freshdesk\Model\Ticket as TicketM,
+  Freshdesk\Tool\ModelGenerator;
+
+$url = 'https://' . $KEY . ':X@chester.freshdesk.com';
+$conf = new Connection($url);
+
+//basic/general rest calls
+$fd = new Rest($conf);
+//get ticket, this call will be removed from Rest class & moved to Ticket class
+//$json = $fd->getSingleTicket(1701);
+//print_r($json);
+//for ticket-calls:
+$t = new Ticket($conf);
+//create new ticket
+$model = new TicketM(
+    array(
+        'description'   => 'Ignore this ticket, it is a test',
+        'subject'       => 'API-test',
+        'email'         => 'foo@bar.com'
+    )
+);
+//create new ticket, basic example
+$t->createNewTicket($model);

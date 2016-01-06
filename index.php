fbad1ec4ac49b179629e1f8b29a702acd731f558
Ben Brittain <ben@brittain.org>
2016-01-06 13:53:39 -0500

    table

--- index.php
+++ index.php
@@ -8,5 +8,33 @@
       <p>Problem: <input type="text" name="problem" /></p>
       <p><input type="submit" /></p>
     </form>
+
+    Current Open Tickets:
+      <?php
+        require 'vendor/autoload.php';
+        include 'config.php';
+        use Freshdesk\Config\Connection,
+          Freshdesk\Rest,
+          Freshdesk\Ticket,
+          Freshdesk\Model\Contact,
+          Freshdesk\Model\Ticket as TicketM,
+          Freshdesk\Tool\ModelGenerator;
+
+        $url = 'https://' . $KEY . ':X@chester.freshdesk.com';
+        $conf = new Connection($url);
+        $fd = new Rest($conf);
+        $tckts = $fd->getAllTickets(0, 'new_and_my_open');
+        echo('<table>');
+        foreach ($tckts as &$tckt) {
+          $requester = $tckt->requester_name;
+          $desc = $tckt->description;
+          $title = $tckt->subject;
+          echo('<tr>');
+          echo('<td>' . $requester . '</td>');
+          echo('<td>' . $title . '</td>');
+          echo('</tr>');
+        }
+        echo('</table>');
+      ?>
  </body>
 </html>

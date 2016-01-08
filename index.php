47d1666e4ada9e1c5af0927ec1492b61cc9d8e14
CCoffie <CalebCoffie@gmail.com>
2016-01-08 12:26:57 -0500

    more progress with AJAX

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html lang="en">
   <head>
     <title>RIT CIS Ticketing System</title>
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js" ></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
@@ -10,8 +11,9 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6 col-md-offset-3" style="background-color: rgb(243,110,33);color: white;margin-top: 50pt;">
+
           <h1>Submit a New Ticket</h1>
-          <form action="newticket.php" method="post">
+          <form id="ticket-form">
             <div class="form-group">
               <label>Email address</label>
               <input type="email" class="form-control" name="email" placeholder="Email">
@@ -37,11 +39,57 @@
               <label>Location</label>
               <input class="form-control" name="location" placeholder="Location"></textarea>
             </div>
-            <button type="submit" class="btn btn-default">Submit</button>
+            <div class="row">
+              <div class="col-md-6">
+                <button id="submit" class="btn btn-default">Submit</button>
+              </div>
+              <div class="col-md-6 text-right">
+                <a href="opentickets.php" role="button" class="btn btn-default">View Open Tickets</a>
+              </div>
+            </div>
           </form>
+
+          <!-- Modal -->
+          <div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                  <h4 class="modal-title" id="myModalLabel">Ticket Submitted</h4>
+                </div>
+                <div class="modal-body">
+                  The ticket has successfully been submitted!
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <footer class="footer">
              <p>© 2015 CIS Student System Administrators</p>
           </footer>
+
+          <script type="text/javascript">
+            $(document).ready(function(){
+              $("#submit").click(function(){
+                console.log("Did I get here?")
+              // $.ajax({
+              //   type: 'POST',
+              //   url: "newticket.php",
+              //   data: $('#ticket-form').serialize(),
+              //   success: function(response) {
+              //     console.log("Submitted ticket!");
+              //     $('#successModal').modal(show);
+              //   },
+              //   error: function() {
+              //     alert("There was an error submitting the ticket");
+              //   }
+              //  });
+             });
+           });
+          </script>
         </div>
       </div>
     </div>
